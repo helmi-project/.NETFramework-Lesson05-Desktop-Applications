876ad06b62e737229b9415b575966bb9b1f53401
Helmi Hartanto <helmihartanto.project@gmail.com>
2021-01-04 14:19:02 +0700

    Add project files.

---
+++ UsingVisualInheritance/InheritedForm.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace UsingVisualInheritance
+{
+    partial class InheritedForm : UsingVisualInheritance.Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.Text = "Inherited Form";
+            // 
+            // InheritedForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(284, 91);
+            this.Name = "InheritedForm";
+            this.Load += new System.EventHandler(this.InheritedForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ UsingVisualInheritance/InheritedForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UsingVisualInheritance
+{
+    public partial class InheritedForm : UsingVisualInheritance.Form1
+    {
+        public InheritedForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InheritedForm_Load(object sender, EventArgs e)
+        {
+            label1.Text = dateTimePicker1.Value.ToLongDateString();
+        }
+    }
+}
